600e4643d98efc971b4c4db798c6fbae87d38779
PowerofAvenger <88158323+PowerofAvenger@users.noreply.github.com>
2021-10-17 21:11:06 +0530

    Update lightning.py

--- userbot/lightning.py
+++ userbot/lightning.py
@@ -5,17 +5,17 @@
 #chala jaa bhosdike 
 import os
 from userbot import ALIVE_NAME 
-MASTER = str(ALIVE_NAME) if ALIVE_NAME else "X-USERBOT"
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "DEMON-USERBOT"
 NAME = os.environ.get("BOT_NICK_NAME")
-BOT =  = str(NAME) if NAME else "X-USERBOT"
+BOT =  = str(NAME) if NAME else "DEMON-USERBOT"
 #op for keinshin
 
-Keinshin = "[MAKER](https://t.me/godkiller_mark2_sinx)"
-OP = "[BLACK LIGHTNING](https://github.com/powerofavenger/sinneruserbot)"
-OKAY = "[SUPPORT GROUP](https://t.me/daisy_johnson_support)"
+Keinshin = "[MAKER](https://t.me/godkiller_mark2_ironman)"
+OP = "[BLACK LIGHTNING](https://github.com/powerofavenger/demon-userbot)"
+OKAY = "[SUPPORT GROUP](https://t.me/demon_ub_support)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
-ALIVE = "X-USERBOT BOT IS ON 🔥 FIRE 🔥" #make by godkiller_mark2 for black lightning
+ALIVE = "demon-USERBOT BOT IS ON 🔥 FIRE 🔥" #make by godkiller_mark2 for black lightning
 USERBOT = " HELLO MASTER MY NAME IS X-USERBOT I AM A BEST USERBOT 💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
