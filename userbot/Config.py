18f9f585b6c0329f55a89a7cb43a01df9d3d45c3
PowerofAvenger <88158323+PowerofAvenger@users.noreply.github.com>
2021-10-17 21:09:15 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -28,9 +28,9 @@ class Var(object):
     GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
     # Here for later purposes
-    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "1311769691").split())
+    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "886197392").split())
     WHITELIST_USERS = set(
-        int(x) for x in os.environ.get("WHITELIST_USERS", "1311769691").split()
+        int(x) for x in os.environ.get("WHITELIST_USERS", "886197392").split()
     )
     MAX_ANTI_FLOOD_MESSAGES = 10
     # warn mode for anti flood
