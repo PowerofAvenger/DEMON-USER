87e2d72c82eafde7cde65ab8f22134f83164b83f
PowerofAvenger <88158323+PowerofAvenger@users.noreply.github.com>
2021-10-30 09:07:12 +0530

    Update lightning.py

--- userbot/lightning.py
+++ userbot/lightning.py
@@ -1,5 +1,5 @@
 #kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
-#make by https://t.me/godkiller_mark2_sinx for chutiapa 🤔😑 you dont kang this 
+#make by https://t.me/dooms_day_xd for chutiapa 🤔😑 you dont kang this 
 #okay to ab isme cheeze bharte h 
 #if you read only this then okay else
 #chala jaa bhosdike 
@@ -11,12 +11,12 @@ BOT =  = str(NAME) if NAME else "DEMON-USERBOT"
 #op for keinshin
 
 Keinshin = "[MAKER](https://t.me/godkiller_mark2_ironman)"
-OP = "[BLACK LIGHTNING](https://github.com/powerofavenger/demon-userbot)"
+OP = "[Demon LIGHTNING](https://github.com/powerofavenger/demon-userbot)"
 OKAY = "[SUPPORT GROUP](https://t.me/demon_ub_support)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
 ALIVE = "demon-USERBOT BOT IS ON 🔥 FIRE 🔥" #make by godkiller_mark2 for black lightning
-USERBOT = " HELLO MASTER MY NAME IS X-USERBOT I AM A BEST USERBOT 💝"
+USERBOT = " HELLO MASTER MY NAME IS Demon-USERBOT I AM A BEST USERBOT 💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
 #aur aage add kruga abhi busy okay 🤔
